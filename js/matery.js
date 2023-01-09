6a6cdda2aa932d2d9b5ba921815ba857466f14a8
quanquan170 <1480105462@qq.com>
2023-01-09 22:04:51 +0800

    Site updated: 2023-01-09 22:04:48

--- js/matery.js
+++ js/matery.js
@@ -172,12 +172,12 @@ $(function () {
 });
 
 //黑夜模式提醒开启功能
-setTimeout(function () {
-    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
-        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
-        M.toast({ html: toastHTML })
-    }
-}, 2200);
+// setTimeout(function () {
+//     if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+//         // let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
+//         // M.toast({ html: toastHTML })
+//     }
+// }, 2200);
 
 //黑夜模式判断
 if (localStorage.getItem('isDark') === '1') {
